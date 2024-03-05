f22441bbfbc5c807c9dfbf76c180bf22133eee70
Saravanan <saransri13@gmail.com>
2024-03-05 20:09:38 +0530

    Added the about us header content

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,32 @@ const About = () => {
   return (
     <Layout>
       <>
+        {/* Page Header Start */}
+        <div
+          className="container-fluid page-header py-5 mb-5 wow fadeIn"
+          data-wow-delay="0.1s"
+        >
+          <div className="container text-center py-5">
+            <h1 className="display-3 text-white mb-4 animated slideInDown">
+              About Us
+            </h1>
+            <nav aria-label="breadcrumb animated slideInDown">
+              <ol className="breadcrumb justify-content-center mb-0">
+                <li className="breadcrumb-item">
+                  <a href="#">Home</a>
+                </li>
+                <li className="breadcrumb-item">
+                  <a href="#">Pages</a>
+                </li>
+                <li className="breadcrumb-item active" aria-current="page">
+                  About
+                </li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+        {/* Page Header End */}
+
         {/* About Start */}
         <div className="container-xxl py-5">
           <div className="container">
