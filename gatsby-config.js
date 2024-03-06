48bab10b07e8d9faae961d4c67dbdc3ae9dac8ce
Saravanan <saransri13@gmail.com>
2024-03-06 14:52:15 +0530

    Disable the packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,36 +7,35 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        // The unique name for each instance
-        name: `pages`,
-        // Path to the directory
-        path: `${__dirname}/src/pages/`,
-      },
-    },
-
-    /* Product md files listed */
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        // The unique name for each instance
-        name: `products`,
-        // Path to the directory
-        path: `${__dirname}/src/products/`,
-      },
-    },
-
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        // The unique name for each instance
-        name: `projects`,
-        // Path to the directory
-        path: `${__dirname}/src/projects/`,
-      },
-    },
+    // "gatsby-transformer-remark",
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     // The unique name for each instance
+    //     name: `pages`,
+    //     // Path to the directory
+    //     path: `${__dirname}/src/pages/`,
+    //   },
+    // },
+    // /* Product md files listed */
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     // The unique name for each instance
+    //     name: `products`,
+    //     // Path to the directory
+    //     path: `${__dirname}/src/products/`,
+    //   },
+    // },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     // The unique name for each instance
+    //     name: `projects`,
+    //     // Path to the directory
+    //     path: `${__dirname}/src/projects/`,
+    //   },
+    // },
   ],
   siteMetadata: {
     title: "Product demo 4",
